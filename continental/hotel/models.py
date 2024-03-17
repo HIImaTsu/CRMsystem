63735945e2a8557a61f05695fe267d55655fcd75
Rakhat <200103319@stu.sdu.edu.kz>
2024-03-17 14:06:40 +0500

    миграция таблиц и создание методов

--- continental/hotel/models.py
+++ continental/hotel/models.py
@@ -10,38 +10,34 @@ class Guest(models.Model):
 class Hotel(models.Model):
     name = models.CharField(max_length=255)
 
-# class Staff(models.Model):
-#     full_name = models.CharField(max_length=255)
-#     position = models.CharField(max_length=100)
-#     phone = models.CharField(max_length=20)
-
-class RoomType:
+class RoomType(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
     capacity = models.IntegerField()
 
-class Booking:
+class Booking(models.Model):
     guest_id = models.ForeignKey('Guest', on_delete=models.PROTECT)
+    # рассмотреть тип связывания этой таблицы с другими
     room_id = models.ForeignKey('Room', on_delete=models.PROTECT)
     checkin_date = models.DateTimeField()
     checkout_date = models.DateTimeField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-class Room:
+class Room(models.Model):
     hotel_id = models.ForeignKey('Hotel', on_delete=models.PROTECT)
     type_id = models.ForeignKey('RoomType', on_delete=models.PROTECT)
     status = models.CharField(max_length=20)
     room_number = models.IntegerField()
 
-class HouseKeeping:
+class HouseKeeping(models.Model):
     room_id = models.ForeignKey('Room', on_delete=models.PROTECT)
     date = models.DateTimeField()
     status = models.CharField(max_length=20)
     checkin_time = models.DateTimeField()
     checkout_time = models.DateTimeField()
 
-class Payment:
+class Payment(models.Model):
     booking_id = models.ForeignKey('Booking', on_delete=models.PROTECT)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateTimeField()
--- continental/hotel/views.py
+++ continental/hotel/views.py
@@ -4,32 +4,34 @@ from django.utils import timezone
 from .models import *
 from .forms import *
 
+booking = Booking.objects.all()
+
 def index(request):
-    return HttpResponse("Страница входа Continental")
+    return HttpResponse("Страница Continental")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
-def addguest(request):
-    if request.method == 'POST':
-        form = AddGuestForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddGuestForm()
-
 def login(request):
-    return render(request, 'index.html')
+    return render(request, 'hotel/index.html')
 
 def booking(request):
-    return render(request, 'booking.html')
+    return render(request, 'hotel/booking.html')
 
-def addbooking(request):
-    if request.method == 'POST':
-        form = AddBookingForm(request.POST)
-        if form.is_valid():
-            form.save(commit=False)
-            return redirect('booking')
-    else:
-        form = AddBookingForm()
+# def addguest(request):
+#     if request.method == 'POST':
+#         form = AddGuestForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddGuestForm()
+#
+# def addbooking(request):
+#     if request.method == 'POST':
+#         form = AddBookingForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('booking')
+#     else:
+#         form = AddBookingForm()
