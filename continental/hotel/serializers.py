34810193ec335bb7edab5e83ea162c78938cba7e
Rakhat <200103319@stu.sdu.edu.kz>
2024-06-13 20:27:42 +0500

    final preparations

--- continental/hotel/serializers.py
+++ continental/hotel/serializers.py
@@ -2,17 +2,24 @@
 from rest_framework import serializers
 from .models import *
 
+class GuestProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GuestProfile
+        fields = ['guest_balance']
+
 class GuestSerializer(serializers.ModelSerializer):
+    profile = GuestProfileSerializer()
+
     class Meta:
         model = Guest
-        fields = ['first_name', 'last_name', 'phone']
+        fields = ['first_name', 'last_name', 'phone', 'profile']
 
 class BookingSerializer(serializers.ModelSerializer):
     guest_info = GuestSerializer(source='guest')
 
     class Meta:
         model = Booking
-        fields = ['guest_info', 'checkin_date', 'checkout_date', 'room']
+        fields = ['guest_info', 'checkin_date', 'checkout_date', 'room', 'total_price']
 
 
 
